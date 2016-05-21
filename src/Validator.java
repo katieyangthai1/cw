510fc61bafdbd83667115ef55480b2b034a440ee
KatieYang <katieyangthai1@gmail.com>
2016-05-20 18:37:13 -0700

    cw1

--- src/Validator.java
+++ src/Validator.java
@@ -1,5 +1,4 @@
-public class Validator
-{
+public class Validator {
     private int[] valid = { 5658845, 4520125, 7895122, 8777541,
             8451277, 1302850, 8080152, 4562555,
             5552012, 5050552, 7825877, 1250255,
